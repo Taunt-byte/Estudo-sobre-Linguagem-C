b7b82a7cebacd1f587467399f246657d6c0427c6
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-05-18 14:03:39 -0300

    Update Lista de exercicios
    Para fins de organização eu criei uma nova pasta para os exercícios corrigidos.

--- Lista-de-exercicios/Exercicios-corrigidos/Lista1/Exercicio1.c
+++ Lista-de-exercicios/Exercicios-corrigidos/Lista1/Exercicio1.c
@@ -8,16 +8,16 @@
 int main() /*Início */
 {
 	float salarioAtual, salarioNovo; /* Declaração de variáveis (Float = real) */
-	
+
 	/*Entrada de dados */
 	printf("Informe o salário atual do funcionário:");
 	scanf("%f",&salarioAtual);
-		
+
 	/*Processamento do novo salário */
 	salarioNovo = salarioAtual * 1.25;
-	
+
 	/*Saída de dados - Resultado */
 	printf("\nO novo salário = %f",salarioNovo);
-	
+
 	return(0);
 } /*Fim */
